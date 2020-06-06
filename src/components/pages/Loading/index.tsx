9cbe9d8887ed39925c336c3ad63e25d5cf862cb8
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-06-06 10:15:40 +0900

    [update] 各画面に色を入れ込む

--- src/components/pages/Loading/index.tsx
+++ src/components/pages/Loading/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import * as UiContext from '../../../contexts/ui';
+import { COLOR } from '../../../constants/theme';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: COLOR.MAIN,
+  },
+  text: {
+    color: COLOR.WHITE,
   },
 });
 
@@ -23,7 +28,7 @@ function ChangeStateButton(props: { state: UiContext.Status }) {
 export default function Loading() {
   return (
     <View style={styles.container}>
-      <Text>Loading</Text>
+      <Text style={styles.text}>Loading</Text>
       <ChangeStateButton state={UiContext.Status.AUTHORIZED} />
       <ChangeStateButton state={UiContext.Status.UN_AUTHORIZED} />
       <ChangeStateButton state={UiContext.Status.FIRST_OPEN} />
