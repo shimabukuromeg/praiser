3219857b58796e985e3433a5472ba1b252d336c8
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-06-08 08:00:13 +0900

    [update] Sign inの作成

---
+++ src/components/pages/SignIn/SignInWithGoogle.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Button from '../../atoms/Button';
+import { Context, Status } from '../../../contexts/ui';
+export default function SignInWithGoogle() {
+  const { setApplicationState } = React.useContext(Context);
+  return <Button onPress={() => setApplicationState(Status.AUTHORIZED)} icon="google" label="Sign In with Google" />;
+}
--- src/components/pages/SignIn/index.tsx
+++ src/components/pages/SignIn/index.tsx
@@ -1,23 +1,61 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { useControlledComponent } from '../../../lib/hooks';
+import { Text, Button, dismiss, TextField } from '../../atoms';
+import SignInWithGoogle from './SignInWithGoogle';
 import { Context, Status } from '../../../contexts/ui';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  text: {
+    marginVertical: 20,
+  },
+  button: {
+    marginTop: 50,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
   },
 });
 
 export default function SignIn() {
   const { setApplicationState } = React.useContext(Context);
+  const mailAddress = useControlledComponent('');
+  const password = useControlledComponent('');
   return (
-    <View style={styles.container}>
-      <Text>SignIn</Text>
-      <TouchableOpacity onPress={() => setApplicationState(Status.AUTHORIZED)}>
-        <Text>Sign IN</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableWithoutFeedback onPress={dismiss}>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <TextField
+            label="email"
+            value={mailAddress.value}
+            onChangeText={mailAddress.onChangeText}
+            style={styles.text}
+            autoCompleteType="email"
+          />
+          <TextField
+            label="password"
+            value={password.value}
+            onChangeText={password.onChangeText}
+            style={styles.text}
+            autoCompleteType="password"
+          />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <SignInWithGoogle />
+          <Button onPress={() => setApplicationState(Status.AUTHORIZED)} label="SignIn" style={styles.button} />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
