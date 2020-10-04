145ead99ab3ab5159d24fa9bc4f6cdcf78f7a62b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-10-04 09:45:19 +0900

    [update] detail画面完了

--- src/components/pages/Detail/index.tsx
+++ src/components/pages/Detail/index.tsx
@@ -1,18 +1,72 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+import { Button, TextField } from '../../atoms';
+import { useControlledComponent } from '../../../lib/hooks';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 10,
+  },
+  textField: {
+    marginVertical: 10,
+  },
+  button: {
+    marginTop: 20,
   },
 });
 
+interface TodoEditActions {
+  changeTodo: (
+    id: string,
+    newValue: {
+      title: string;
+      detail: string;
+    },
+  ) => void;
+}
+
+interface Props {
+  actions: TodoEditActions;
+}
+
+interface Params {
+  id: string;
+  isEditable: boolean;
+  title: string;
+  detail: string;
+}
+
 export default function Detail() {
+  const { goBack } = useNavigation();
+  const { params } = useRoute<RouteProp<Record<string, Params>, string>>();
+  const { isEditable, title: titleInitialValue, detail: detailInitialValue } = params;
+
+  const title = useControlledComponent(titleInitialValue);
+  const detail = useControlledComponent(detailInitialValue);
+
+  const onSubmit = React.useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
     <View style={styles.container}>
-      <Text>Detail</Text>
+      <TextField
+        disabled={!isEditable}
+        label="title"
+        value={title.value}
+        onChangeText={title.onChangeText}
+        style={styles.textField}
+      />
+      <TextField
+        disabled={!isEditable}
+        label="detail"
+        value={detail.value}
+        onChangeText={detail.onChangeText}
+        style={styles.textField}
+      />
+      {isEditable && <Button onPress={onSubmit} label="Submit" style={styles.button} />}
     </View>
   );
 }
