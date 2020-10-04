f1c60f6279cd23a7381540804ddef390e68f5932
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-10-04 12:02:57 +0900

    Statistics画面作成

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,3 +5,5 @@ export { default as TextField, dismiss } from './TextField';
 export { default as IconButton } from './IconButton';
 export { default as Avatar } from './Avatar';
 export { default as LabelValueContainer } from './LabelValueContainer';
+export { default as Progress } from './Progress';
+export { default as HeaderText } from './HeaderText';
